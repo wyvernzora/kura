d1b247675868d74229053d4a8730355f87b1afa2
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2026-07-23 12:24:13 -0700

    chore(n8n): stop tracking kura package build artifacts

--- integrations/n8n/kura/dist/credentials/KuraApi.credentials.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Icon, ICredentialTestRequest, ICredentialType, INodeProperties } from 'n8n-workflow';
-export declare class KuraApi implements ICredentialType {
-    name: string;
-    displayName: string;
-    documentationUrl: string;
-    icon: Icon;
-    properties: INodeProperties[];
-    test: ICredentialTestRequest;
-}
